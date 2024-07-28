73f16ae23d26cb502dbc1e4db6cd687e1a5f0040
gkopoin <gab270b@gmail.com>
2024-07-28 23:32:53 +0200

    Move function sort by time in new file

---
+++ internal/ls-time.go
@@ -0,0 +1,14 @@
+package ls
+
+import (
+	"io/fs"
+	"sort"
+)
+
+func SortTime(fileInfos []fs.FileInfo, opts Option) {
+	if opts.Time {
+		sort.Slice(fileInfos, func(i int, j int) bool {
+			return fileInfos[i].ModTime().Before((fileInfos[j].ModTime()))
+		})
+	}
+}
