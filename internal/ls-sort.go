9553704c6454382cd61f04e973a9a8a74be541e4
gkopoin <gab270b@gmail.com>
2024-07-28 23:32:14 +0200

    New function to sort only alphabetic characters

--- internal/ls-sort.go
+++ internal/ls-sort.go
@@ -2,22 +2,25 @@ package ls
 
 import (
 	"io/fs"
+	"regexp"
 	"sort"
 )
 
+// Fonction tri par nom dans l'ordre décroissant si reverse est activé ou bien croissant quand ce n'est pas le cas.
 func SortDisplay(fileInfos []fs.FileInfo, opts Option) {
 	if opts.Reverse {
 		sort.Slice(fileInfos, func(i int, j int) bool {
-			return fileInfos[i].Name() > fileInfos[j].Name()
+			return CleanName(fileInfos[i].Name()) > CleanName(fileInfos[j].Name())
 		})
 	} else {
 		sort.Slice(fileInfos, func(i int, j int) bool {
-			return fileInfos[i].Name() < fileInfos[j].Name()
+			return CleanName(fileInfos[i].Name()) < CleanName(fileInfos[j].Name())
 		})
 	}
-	if opts.Time {
-		sort.Slice(fileInfos, func(i int, j int) bool {
-			return fileInfos[i].ModTime().After(fileInfos[j].ModTime())
-		})
-	}
+}
+
+// Fonction pour que tout ce qui n'est pas des caractères alphabétiques ne soit pas pris en compte.
+func CleanName(name string) string {
+	re := regexp.MustCompile("[^a-zA-Z0-9]+")
+	return re.ReplaceAllString(name, "")
 }
