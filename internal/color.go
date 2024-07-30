3b8553e88b7a647147f1dd8dfa0cd06c61cee2fb
ejean <esteban-jean.pro@hotmail.com>
2024-07-30 13:01:43 +0200

    Colors for SetGID and SetUID supported

--- internal/color.go
+++ internal/color.go
@@ -6,26 +6,40 @@ import (
 )
 
 const (
-	ColorReset   = "\033[0m"
-	ColorBold    = "\033[1m"
-	ColorBlue    = "\033[34m"
-	ColorGreen   = "\033[32m"
-	ColorCyan    = "\033[36m"
-	ColorRed     = "\033[31m"
-	ColorYellow  = "\033[33m"
-	ColorMagenta = "\033[35m"
+	ColorReset     = "\033[0m"
+	ColorBold      = "\033[1m"
+	ColorFGBlack   = "\033[30m"
+	ColorFGRed     = "\033[31m"
+	ColorFGGreen   = "\033[32m"
+	ColorFGYellow  = "\033[33m"
+	ColorFGBlue    = "\033[34m"
+	ColorFGMagenta = "\033[35m"
+	ColorFGCyan    = "\033[36m"
+	ColorFGWhite   = "\033[37m"
+	ColorBGBlack   = "\033[40m"
+	ColorBGRed     = "\033[41m"
+	ColorBGGreen   = "\033[42m"
+	ColorBGYellow  = "\033[43m"
+	ColorBGBlue    = "\033[44m"
+	ColorBGMagenta = "\033[45m"
+	ColorBGCyan    = "\033[46m"
+	ColorBGWhite   = "\033[47m"
 )
 
 func Colorize(name string, info os.FileInfo) string {
 	var color string
 	if info.IsDir() {
-		color = ColorBold + ColorBlue
+		color = ColorBold + ColorFGBlue
 	} else if strings.HasSuffix(name, ".tar") || strings.HasSuffix(name, ".gz") || strings.HasSuffix(name, ".zip") {
-		color = ColorBold + ColorRed
+		color = ColorBold + ColorFGRed
+	} else if (info.Mode() & os.ModeSetuid) != 0 {
+		color = ColorFGWhite + ColorBGRed
+	} else if (info.Mode() & os.ModeSetgid) != 0 {
+		color = ColorFGBlack + ColorBGYellow
 	} else if (info.Mode() & os.ModeSymlink) != 0 {
-		color = ColorBold + ColorCyan
+		color = ColorBold + ColorFGCyan
 	} else if (info.Mode() & 0100) != 0 {
-		color = ColorBold + ColorGreen
+		color = ColorBold + ColorFGGreen
 	}
 	return color + name + ColorReset
 }
